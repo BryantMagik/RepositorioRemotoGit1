cd2f761ff26695b1d299684308797f1e5289747f
bryan <bryan@LAPTOP-PG6I6678>
2023-05-05 14:03:42 +0200

    cuarto commi

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejemplo de control de versiones con Git");
-		System.out.println("Ejemplo de segundo control de versiones con Git ver2");
+		System.out.println("Ejemplo de segundo control de versiones con Git");
+		System.out.println("Pito");
 
 	}
 }
